74da6c31af6e92b3b9c5fe3f9c95d7c3b8813765
MarxBro <allthemarxbrothers@gmail.com>
2015-10-12 03:40:30 -0300

    bring rss back: joy of the people VI

--- rss.php
+++ rss.php
@@ -2,12 +2,12 @@
 include_once("functions.inc.php");
 include_once("atamishky.conf.inc.php");
 
-$xmlfile='catalogo.xml';
-$xslfile='rss.xsl';
-$params['atamishkyhome'] = $atamishky_HOME;
-$params['pagetitle'] = $PAGE_TITLE;
-$params['title'] = $PAGE_TITLE;
-$params['pagedescription'] = $PAGE_DESCRIPTION;
-$params['favicon'] = $FAVICON;
+$xmlfile                    ='catalogo.xml';
+$xslfile                    ='rss.xsl';
+$params['atamishkyhome']    = $atamishky_HOME;
+$params['pagetitle']        = $PAGE_TITLE;
+$params['title']            = $PAGE_TITLE;
+$params['pagedescription']  = $PAGE_DESCRIPTION;
+$params['favicon']          = $FAVICON;
 echo transform($xmlfile, $xslfile, $params);
 ?>
