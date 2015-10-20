d71cd416e3c82a14c4440b76155e58ea5828bad9
MarxBro <allthemarxbrothers@gmail.com>
2015-10-20 00:42:32 -0300

    APA e ISO ocultar - eskeleton - md5 worked

--- functions.inc.php
+++ functions.inc.php
@@ -101,7 +101,7 @@ function do_hash_seguridad_vendehumo() {
             $line           = str_replace("\n", '', $line);
             $matches_rgx    = explode(' ',$line);
             $md5_rgx        = $matches_rgx[0];
-            $nn_rgx         = $matches_rgx[1];
+            $nn_rgx         = "lib/" . $matches_rgx[1];
             $MD5s[$nn_rgx]  = $md5_rgx;
         }
     } else {
