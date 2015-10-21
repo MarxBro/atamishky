2ece9a09d8d823005bf5afff47c91aa499d935ca
MarxBro <allthemarxbrothers@gmail.com>
2015-10-21 02:18:12 -0300

    proper Doc, merge soon, stable

--- ayuda.php
+++ ayuda.php
@@ -3,7 +3,11 @@
 <div class="title">Ayuda</div>
 <div class="entrybody">
 
+<h2>Acerca de la forma de uso del buscador</h2>
+
 <p>Para comenzar una busqueda, utilice la solapa de la izquierda y seleccione el criterio de filtrado.</p>
+
+<h2>Criterios de busqueda predefinidos</h2>
 
 <p>Puede seleccionar alguno de los siguientes criterios:</p>
 
@@ -18,15 +22,55 @@
 <li>Otros</li>
 </ul>
 
-<p>Los primeros cuatro criterios afectan a cualquier tipo de material: es indistinto si es un libro o una pelicula.</p>
+<p>Los primeros cuatro criterios afectan a cualquier tipo de material: es
+indistinto si es un libro o una pelicula.</p>
 
-<p>Si la opción <strong>Titulo</strong> es utilizada como criterio, se buscaran todos los titulos que coincidan con el texto introducido. Similarmente, si se utiliza el texto buscado como parte de la descripcion de algun material bibliografico, bajao una busqueda del mismo criterio, el resultado aparecera en la lista.</p>
+<p>Si la opción <strong>Titulo</strong> es utilizada como criterio, se buscaran
+todos los titulos que coincidan con el texto introducido. Similarmente, si se
+utiliza el texto buscado como parte de la descripcion de algun material
+bibliografico, bajao una busqueda del mismo criterio, el resultado aparecera en
+la lista.</p>
 
-<p>La opcion <strong>Autor</strong> filtra en la lista total de autores, por los nombres y/o apellidos que lo incluyan. Asi, "ado" incluye a Adorno y a Adolfo Bioy Casare, por ejemplo.</p>
+<p>La opcion <strong>Autor</strong> filtra en la lista total de autores, por
+los nombres y/o apellidos que lo incluyan. Asi, "ado" incluye a Adorno y a
+Adolfo Bioy Casares, por ejemplo.</p>
 
-<p>Los ulttimos cuatro criterios, en cambio, buscan cualquier tipo de texto en los  materiales que concuerden con el tipo: por ejemplo, una busqueda de "music" en el criterio por defecto (TODOS) va a mostrar como resultados validos materiales que pudiesen ser libros o musica. En cambio, si se utiliza el criterio "Libros", todas las entradas que no sean de este tipo son filtradas de antemano: busca únicamente en el caso de las citas que pertenezcan al mismo tipo.</p>
+<p>Los ulttimos cuatro criterios, en cambio, <i>buscan cualquier texto en
+los  materiales que concuerden con el tipo</i>: por ejemplo, una busqueda de
+"music" en el criterio por defecto (TODOS) va a mostrar como resultados validos
+materiales que pudiesen ser libros o musica. En cambio, si se utiliza el
+criterio "Libros", todas las entradas que no sean de este tipo son filtradas de
+antemano: busca únicamente en el caso de las citas que pertenezcan al mismo
+tipo.</p>
 
-<p>Adicionalmente, es posible utilizar -casi- cualquier criterio desde los titulos como filtrado: autores, titulos, idomas, soporte, editoriales... Alcanza con cliquear en el enlace para convertir al elemento en un filtro.</p>
+<h2>Criterios de busqueda dinamicos</h2>
+
+<p>Adicionalmente, <strong>es posible utilizar -casi- cualquier criterio como 
+filtrado dinamico</strong>: autores, titulos, idomas, soporte, editoriales...
+Alcanza con cliquear en el enlace para convertir al elemento en un filtro.</p>
+
+<p>En el encabezado de la página se especifica el tipo de filtrado actual.</p>
+
+<h2>Criterios de organizacion del catalogo adicionales</h2>
+
+<p>Estos son dos (por el momento) y funcionan como colecciones de referencias: una
+categorizacion inmediatamente superior al tipo de material que agrupa varias
+entradas y funciona como conjunto.</p>
+
+
+<ul>
+    <li>Tag</li>
+    <li>Bibliografia</li>
+</ul>
+
+<p>Los tags o etiquetas, sirven para clasificar cualquier referencia acorde a un
+concepto, que permita relacionar varias entradas.</p>
+
+<p>La opcion <strong>ver todos</strong> muestra la lista de tags completa; los
+tags con mayores ocurrencias dentro del catalogo se ven mas grandes. Para ver
+los tags relacionados a alguna entrada en particular, alcanza con cosultar la
+informacion detallada de la entrada, a través del enlace <strong>mas
+info</strong> de la derecha.</p>
 
 
 </div>
