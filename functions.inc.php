82070aa10d0c0331f41a3ec35cda5a2ad1914fcd
MarxBro <allthemarxbrothers@gmail.com>
2015-10-23 19:42:41 -0300

    probando prestamos engine chiotto I

--- functions.inc.php
+++ functions.inc.php
@@ -158,7 +158,7 @@ function IcanHas_booked_items_array($rtyu) {
 function booked_items_check_status ($librito){
     $archivo_prestamos_mm = "lib/prestamos.sec";
     $booked_stuff_mm = IcanHas_booked_items_array($archivo_prestamos);
-    if (in_array($librito, $booked_stuff_mm){
+    if (in_array($librito, $booked_stuff_mm)){
         return true;
     }
 }
