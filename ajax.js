851637fb49eed108e8310a7ab9d4936854cb9f9d
MarxBro <allthemarxbrothers@gmail.com>
2015-11-01 16:49:24 -0300

    mejorando prestamos divs container

--- ajax.js
+++ ajax.js
@@ -427,6 +427,7 @@ function stateChangedKeywords_prs(){
         var pe  = new DOMParser();
         var Cnt = pe.parseFromString(prestados,"text/xml");
         var add_me = Cnt.getElementsByClassName('entry1');
+        console.log(add_me);
         /*Este div es el contenedor... sip, lo agrego desde aca.*/
         var entradas_prestamos = '<div class="content_pager" id="cntt">';
         for (var i = 0; i < add_me.length; i++){
