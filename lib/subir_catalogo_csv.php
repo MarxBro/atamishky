7a938b9a1dce6a90980271ce2937419e37d8779e
MarxBro <allthemarxbrothers@gmail.com>
2016-06-17 18:41:53 -0300

    prolijeando y backup stuffs

--- lib/subir_catalogo_csv.php
+++ lib/subir_catalogo_csv.php
@@ -7,34 +7,19 @@ $uploadOk = 1;
 $iFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 $ctlg_name = 'catalogo.xml';
 
-// Check if image file is a actual image or fake image
-//if(isset($_POST["submit"])) {
-    //if (mime_content_type($target_file == 'text/plain')){
-        //$uploadOk = 1;
-    //} else {
-        //$uploadOk = 0;
-        //morir(); // EXIT
-    //}
-//}
-
 // Check if file already exists
-//if (file_exists($target_file)) {
+if (file_exists($target_file)) {
     // renombrar el archivo viejo preexistente.
-    //$uploadOk = 0;
-    //morir(); // EXIT
-        //}
+    if (!(respaldame_esto($target_file))){
+        morir();    
+    }
+}
  // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "El archivo es demasiado grande!";
     $uploadOk = 0;
         morir(); // EXIT
  }
-// Allow certain file formats
-//if($iFileType != "csv" ) {
-//echo "Solamente subir archivos  de formato CSV; Intente nuevamente, Gracias.";
-//$uploadOk = 0;
-//morir(); // EXIT
-//}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "El archivo no puede ser subido.";
@@ -57,7 +42,7 @@ function cataloguear (){
     global $target_file;
     $ctlg_name_x = $ctlg_name;
     $target_file_x =  $target_file;
-    if (respaldame_el_catalogo_putin($ctlg_name_x)){
+    if (respaldame_esto($ctlg_name_x)){
         // Ejecutar el script de lib con el catalogo
         $pathy   = './';
         $comando = $pathy . 'atamishky_CSV2XML.pl -f ' . $target_file_x . 
@@ -69,7 +54,7 @@ function cataloguear (){
 }
 
 // Respaldar el catalogo anterior!
-function respaldame_el_catalogo_putin ($nombre_posta){
+function respaldame_esto($nombre_posta){
     $fecha_ahora_strng = time();
     $nombre_backup = $nombre_posta . '_' . $fecha_ahora_strng . '.bk';
     copy($nombre_posta,$nombre_backup);
@@ -79,8 +64,7 @@ function respaldame_el_catalogo_putin ($nombre_posta){
 }
 
 function morir(){
-    echo "ERRORORORORORO";
-    //die "Error en el archivo, Final no feliz!";
+    echo "ERROR!";
 }
 
 ?>
