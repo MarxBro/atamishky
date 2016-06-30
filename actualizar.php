591872f8ac99cf66096f5d4893dae25bc06ab391
MarxBro <allthemarxbrothers@gmail.com>
2016-06-30 16:11:08 -0300

    css de subida csv

--- actualizar.php
+++ actualizar.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="site.css">
+    <title>Actualización Catálogo :: ATAM - UNA</title>
+</head>
 <body>
+<div class="atamishky">
+<h1 style="color: grey;">Actualización Catálogo</h1>
 
+<div class="entry1">
+    <p><b>Utilice un archivo CSV con la estructura correcta.</b>.</p>
+</div>
+
+<div class="whitebox">
+<div class="content">
+<div align="right">
 <form action="lib/subir_catalogo_csv.php" method="post" enctype="multipart/form-data">
-    Subir un archivo CSV:
-    <input type="file" name="fileToUpload" id="fileToUpload" type="file">
-    <input type="submit" value="Subir CSV" name="submit">
+    <fieldset>
+    <legend>Subir un archivo CSV:</legend>
+    <input class="elemento" type="file" name="fileToUpload" id="fileToUpload" type="file">
+            <br/>
+    <input class="elemento" type="submit" value="Subir CSV" name="submit">
+    </fieldset>
  </form>
+</div>
+</div>
+</div>
 
+</div>
 </body>
 </html>
